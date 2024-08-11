3ec1664937e7f666517811b0c5abeff17a12e2eb
Ajijagatsaputra <ajijagat90@gmail.com>
2024-08-11 15:08:23 +0700

    slicing ui gojekapp

---
+++ lib/datas/news.dart
@@ -0,0 +1,7 @@
+import 'package:gojek/models/news.dart';
+
+List<News> news = [
+  News('1.png', 'Makin Seru', 'Aktifkan & Sambungkan GoPay & GoPayLater di Tokopedia'),
+  News('2.png', 'Makin Seru', 'Aktifkan & Sambungin Akun ke Tokopedia, Banyakin Untung'),
+  News('3.png', 'Makin Seru', 'Promo Belanja Online 8.8 : Cashback hingga Rp.100.000'),
+];
